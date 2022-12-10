d863156cfebafb30a310e8c2153411a7d587d092
Ravikiran Reddy Balemla <56499420+kirannani168@users.noreply.github.com>
2022-12-10 06:15:29 +0530

    Checkpoint 2
    Header.js and Header.css files added under ./Common/Header folder

---
+++ src/Common/Header/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Header.css'
+class Header extends Component {
+  render() {
+    return (
+      <div id='MainHeader'>
+            <img alt="HEader Img" src="https://cdn.upgrad.com/uploads/production/286e1f11-1897-4d0c-ab0f-6b2bfc1ce642/logo.svg"/>
+      </div>
+    )
+  }
+}
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Header from './Common/Header/Header';
 import './index.css';
 
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
+    <Header/>
   </React.StrictMode>
 );
 
